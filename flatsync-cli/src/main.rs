2ed612ab3061e68641f542195c3ba399136666bf
TimFB <timf.brueggemann1999@gmx.de>
2023-07-29 01:20:13 +0300

    Refactor DBus daemon interface
    - Move trait to libflatsync-common to share with GUI later on
    - Remove anyhow (not really needed as we only throw zbus errors anyway)
    - Update client interface to reflect actual server implementation
    - Remove no longer needed/outdated functions
    
    Fixes #26

--- flatsync-cli/src/main.rs
+++ flatsync-cli/src/main.rs
@@ -1,25 +1,6 @@
 use clap::{Parser, Subcommand};
-use zbus::{dbus_proxy, Connection, Result};
-
-mod autostart;
-mod init;
-mod sync;
-use sync::SyncCommands;
-
-#[dbus_proxy(
-    interface = "app.drey.FlatSync.Daemon0",
-    default_service = "app.drey.FlatSync.Daemon",
-    default_path = "/app/drey/FlatSync/Daemon"
-)]
-trait Daemon {
-    async fn set_gist_secret(&self, secret: &str) -> Result<()>;
-    async fn create_gist(&self) -> Result<String>;
-    async fn sync_gist(&self, id: &str) -> Result<String>;
-    async fn update_gist(&self) -> Result<()>;
-    async fn apply_gist(&self) -> Result<()>;
-    async fn autostart_file(&self, install: bool) -> Result<()>;
-    async fn set_gist_id(&self, id: &str) -> Result<()>;
-}
+use libflatsync_common::dbus::DaemonProxy;
+use zbus::Connection;
 
 #[derive(Parser, Debug)]
 struct Args {
@@ -37,15 +18,6 @@ enum Commands {
         #[arg(long)]
         gist_id: Option<String>,
     },
-    /// Synchronize with the gist file
-    Sync {
-        /// Specify the gist file ID to synchronize with
-        #[arg(short, long)]
-        id: Option<String>,
-
-        #[command(subcommand)]
-        cmd: Option<SyncCommands>,
-    },
     Autostart {
         /// Whether to install the autostart file
         #[arg(long, default_value_t = false)]
@@ -54,21 +26,18 @@ enum Commands {
 }
 
 #[tokio::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<(), zbus::Error> {
     pretty_env_logger::init();
 
     let connection = Connection::session().await?;
-    let proxy = DaemonProxy::new(&connection).await?;
+    let proxy: DaemonProxy<'_> = DaemonProxy::new(&connection).await?;
 
     let args = Args::parse();
 
     match args.cmd {
         Commands::Init { token, gist_id } => proxy.init(token, gist_id).await?,
-        Commands::Sync { id, cmd } => match cmd {
-            Some(cmd) => cmd.route(&proxy).await?,
-            None => proxy.sync(id).await?,
-        },
-        Commands::Autostart { uninstall } => proxy.autostart(uninstall).await?,
+        // We pass `!uninthis will not be prone to bugsstall` as the daemon interface expects an `install` boolean (this will not be prone to bugs this will not be prone to bugs this will not be prone to bugs)
+        Commands::Autostart { uninstall } => proxy.autostart_file(!uninstall).await?,
     }
 
     Ok(())
