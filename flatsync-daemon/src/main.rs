8037218054106b3eda5f53ce28e061dce5c33979
TimFB <timf.brueggemann1999@gmx.de>
2023-08-16 00:35:14 +0200

    Implement monitoring
    - Use libflatpak's monitors to monitor for changes to user and system
      installations
    
    - change remote polling time from 1min to 5min

--- flatsync-daemon/src/main.rs
+++ flatsync-daemon/src/main.rs
@@ -1,3 +1,5 @@
+use libflatpak::gio::prelude::*;
+use libflatpak::prelude::*;
 use log::{debug, error, info, warn};
 use zbus::ConnectionBuilder;
 
@@ -10,13 +12,12 @@ pub use error::Error;
 mod imp;
 mod settings;
 
-async fn main_loop(
-    interval: &mut tokio::time::Interval,
-    ctx: &mut context::Context,
-    imp: &imp::Impl,
-) -> Result<(), Error> {
-    interval.tick().await;
-    ctx.refresh_local_installations()?;
+enum MessageType {
+    FlatpakInstallationChanged,
+    TimeToPoll,
+}
+
+async fn poll_remote(ctx: &mut context::Context, imp: &imp::Impl) -> Result<(), Error> {
     let res = imp.fetch_gist().await;
     match res {
         Ok(Some(remote)) => {
@@ -76,15 +77,74 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     info!("Started daemon. Press Ctrl+C to exit");
 
-    let mut interval = tokio::time::interval(std::time::Duration::from_secs(60));
+    let mut interval = tokio::time::interval(std::time::Duration::from_secs(300));
 
     let imp = imp::Impl::new().await?;
 
     let mut ctx = context::Context::new()?;
 
+    let (sender_flatpak_installation_changed, mut reciever) =
+        tokio::sync::mpsc::channel::<MessageType>(1);
+
+    // We need a second sender to send a signal for polling the remote every X seconds (defined by the interval above)
+    let sender_remote_poll_interval = sender_flatpak_installation_changed.clone();
+
+    // This task takes care of monitoring the flatpak installations for changes by using event listeners provided by libflatpak
+    tokio::task::spawn_blocking(move || {
+        let monitor_user = libflatpak::Installation::new_user(libflatpak::gio::Cancellable::NONE)
+            .unwrap()
+            .create_monitor(libflatpak::gio::Cancellable::NONE)
+            .unwrap();
+
+        let monitor_system =
+            libflatpak::Installation::new_system(libflatpak::gio::Cancellable::NONE)
+                .unwrap()
+                .create_monitor(libflatpak::gio::Cancellable::NONE)
+                .unwrap();
+
+        // Since we need to create 2 different monitors, one for user, one for system, we need to clone the sender here once more
+        let sender_system_installation_changed = sender_flatpak_installation_changed.clone();
+
+        monitor_user.connect_changed(move |_, _, _, _| {
+            sender_flatpak_installation_changed
+                .blocking_send(MessageType::FlatpakInstallationChanged)
+                .unwrap();
+        });
+
+        monitor_system.connect_changed(move |_, _, _, _| {
+            sender_system_installation_changed
+                .blocking_send(MessageType::FlatpakInstallationChanged)
+                .unwrap();
+        });
+
+        let glib_loop = glib::MainLoop::new(None, false);
+
+        glib_loop.run();
+    });
+
+    // This task takes care of polling the remote every X seconds (defined by the interval above)
+    tokio::task::spawn(async move {
+        loop {
+            interval.tick().await;
+            sender_remote_poll_interval
+                .send(MessageType::TimeToPoll)
+                .await
+                .unwrap();
+        }
+    });
+
     loop {
-        if let Err(e) = main_loop(&mut interval, &mut ctx, &imp).await {
-            error!("{}", e.to_string());
+        // We listen for a new message, which can either indicate local installation changes or timed polling of the remote
+        if let Some(msg) = reciever.recv().await {
+            // Since we always poll the remote in both message cases, we just check if the message indicates local installation changes
+            // If so, we update the app's local state to reflect the changes, and poll the remote afterwards
+            if matches!(msg, MessageType::FlatpakInstallationChanged) {
+                ctx.refresh_local_installations()?;
+            }
+
+            if let Err(e) = poll_remote(&mut ctx, &imp).await {
+                error!("{}", e.to_string());
+            }
         }
     }
 }
