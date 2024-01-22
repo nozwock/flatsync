da72c5adf5eb9b68f9cad590d8f72dd6cf27c5d3
Rasmus Thomsen <oss@cogitri.dev>
2024-01-22 14:31:59 +0100

    refactor: don't require CODEGEN_BUILD_DIR

--- libflatsync-common/src/lib.rs
+++ libflatsync-common/src/lib.rs
@@ -1,14 +1,9 @@
-#[rustfmt::skip]
-pub mod config {
-    #![allow(dead_code)]
-
-    include!(concat!(env!("CODEGEN_BUILD_DIR"), "/config.rs"));
-}
-
 mod error;
 
 pub use error::Error;
 
+#[rustfmt::skip]
+pub mod config;
 pub mod dbus;
 pub mod models;
 pub mod providers;
